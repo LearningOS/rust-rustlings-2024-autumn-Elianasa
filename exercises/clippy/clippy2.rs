2c954c92cc72cfcdd927cc381dba02059e625488
äbc¨ <est@qq.com>
2024-10-17 20:56:13 +0800

    fine

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -2,10 +2,12 @@
 // 
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 
+
+
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    while let Some(x) =option {//for循环不可靠
+    if let Some(x) = option {
         res += x;
     }
     println!("{}", res);
